a2455f4ee53baf02c9a342125b58fea54d9bee68
vamsi <tovamsich@gmail.com>
2017-07-14 11:47:52 +0530

    Locations

--- site/src/main/java/com/mycompany/api/sitedata/controller/LocationController.java
+++ site/src/main/java/com/mycompany/api/sitedata/controller/LocationController.java
@@ -7,20 +7,38 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.myapp.core.beans.CityData;
 import com.myapp.core.beans.RegionData;
-import com.myapp.core.catalog.facades.RegionFacade;
+import com.myapp.core.catalog.facades.LocationFacade;
 
 @Controller
-@RequestMapping(name="/location")
+@RequestMapping(value="/location")
 public class LocationController 
 {
-	@Resource(name="regionFacade")
-	private RegionFacade regionFacade;
+	@Resource(name="locationFacade")
+	private LocationFacade locationFacade;
 	
-	@RequestMapping(value="/regions")
+	@RequestMapping(value="/regions", produces="application/json")
+	@ResponseBody
 	public List<RegionData> getRegions(HttpServletResponse request, HttpServletResponse response)
 	{
-		return regionFacade.getAllRegions();
+		return locationFacade.getAllRegions();
+	}
+	
+	@RequestMapping(value="/regions-startwith", produces="application/json")
+	@ResponseBody
+	public List<RegionData> getRegionsStartWith(@RequestParam("query") String query, HttpServletResponse request, HttpServletResponse response)
+	{
+		return locationFacade.getRegionsStartWith(query);
+	}
+	
+	@RequestMapping(value="/cities-startwith", produces="application/json")
+	@ResponseBody
+	public List<CityData> getCitiesStartWith(@RequestParam("query") String query,HttpServletResponse request, HttpServletResponse response)
+	{
+		return locationFacade.getCitiesStartWith(query);
 	}
 }
