6b075d3a63f91f1dd950f665f2dea07ce2449e17
vamsi <tovamsich@gmail.com>
2017-07-14 11:49:24 +0530

    location service

---
+++ core/src/main/java/com/myapp/core/beans/CityData.java
@@ -0,0 +1,27 @@
+package com.myapp.core.beans;
+
+public class CityData 
+{
+	private String code;
+	private String name;
+	private String stateCode;
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getStateCode() {
+		return stateCode;
+	}
+	public void setStateCode(String stateCode) {
+		this.stateCode = stateCode;
+	}
+	
+}
